c66ad21d269fc2597d6a9b719179d880e4922c7d
Aayush kumar sah <111872525+Aayushsah6969@users.noreply.github.com>
2025-06-13 18:28:12 +0545

    Update db.py

--- Project01/config/db.py
+++ Project01/config/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 
-MONGO_URI="mongodb+srv://aayushsah269:0RbjxUrprp3T342S@cluster0.it7to2e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/AllNotes"
+MONGO_URI="<your db url here>"
 
 conn = MongoClient(MONGO_URI)
